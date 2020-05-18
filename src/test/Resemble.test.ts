027ce05afbe7f347bd241ea85823d7e5a1e84066
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-05-19 00:34:07 +0200

    testing getDiff

--- src/test/Resemble.test.ts
+++ src/test/Resemble.test.ts
@@ -1,4 +1,5 @@
 import Resemble from '../Resemble';
+import * as fs from 'fs';
 
 describe('Test for Resemble error area color output', () => {
 	test('Colors are out of scale', () => {
@@ -48,5 +49,21 @@ describe('Test for Resemble output transparency', () => {
 		resemble.setTransparency(0.4);
 
 		expect(resemble.getTransparencyLevel()).toEqual(0.4);
+	});
+});
+
+describe('Testing Resemblejs comparation with default settings', () => {
+	test('Images are the same', async () => {
+		const resemble = new Resemble();
+
+		const image1 = fs.readFileSync('./src/test/testimage1.png');
+		const image2 = fs.readFileSync('./src/test/testimage1.png');
+
+		const data = await resemble.getDiff(image1, image2);
+
+		// eslint-disable-next-line prettier/prettier
+		const resultBuffer = fs.readFileSync('./src/test/buffer_test_images_equal.txt');
+
+		expect(data.getBuffer()).toEqual(resultBuffer);
 	});
 });

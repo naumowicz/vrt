385d2688748bcf6828cfae7e0f14ce9ddacddfd1
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-05-19 15:47:13 +0200

    fs features

---
+++ src/FileSystem.ts
@@ -0,0 +1,68 @@
+import fs from 'fs';
+import { promises as fsPromises } from 'fs';
+import * as path from 'path';
+
+class Path {
+	public static resolvePath(pathToFile: string): string {
+		return path.posix.resolve(pathToFile);
+	}
+}
+
+class FileSystem {
+	public async writeFile(pathToFile: string, data: Buffer): Promise<boolean> {
+		try {
+			await fsPromises.writeFile(Path.resolvePath(pathToFile), data);
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+		return true;
+	}
+
+	public async readFile(pathToFile: string): Promise<boolean> {
+		try {
+			await fsPromises.readFile(Path.resolvePath(pathToFile));
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+		return true;
+	}
+
+	public async checkAvailability(pathToFile: string): Promise<boolean> {
+		try {
+			fsPromises.access(
+				Path.resolvePath(pathToFile),
+				fs.constants.R_OK | fs.constants.W_OK,
+			);
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+		return true;
+	}
+
+	public async deleteFile(pathToFile: string): Promise<boolean> {
+		try {
+			fsPromises.unlink(Path.resolvePath(pathToFile));
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+		return true;
+	}
+
+	public async removeFolderRecursively(
+		pathToFolder: string,
+	): Promise<boolean> {
+		try {
+			fsPromises.rmdir(pathToFolder, { recursive: true });
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+		return true;
+	}
+}
+
+export default FileSystem;

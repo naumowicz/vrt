b688b1e660610b99e8abc8633a6edbdd48ca10ad
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-05-27 22:41:52 +0200

    test for properly configured scenario

--- src/test/Scenario.test.ts
+++ src/test/Scenario.test.ts
@@ -22,3 +22,21 @@ describe('Checking scenario availability', () => {
 		expect(result).toEqual(false);
 	});
 });
+
+describe('Checking if scenario is properly configured', () => {
+	test('Scenario properly configured', async () => {
+		const imagesToAnalyze = [
+			'./scenarios/test/test1/baseline/1.png',
+			'./scenarios/test/test2/baseline/2.png',
+		];
+		const steps = [['google.com'], ['google.com/images']];
+
+		const fileSystem = new FileSystem();
+		const scenario = new Scenario();
+		await scenario.loadScenario(
+			'./src/test/scenarioTest/scenario1.txt',
+			fileSystem,
+		);
+		expect(scenario.getScenarioSteps()).toEqual({ steps, imagesToAnalyze });
+	});
+});

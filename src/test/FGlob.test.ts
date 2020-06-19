552bca269410dd1f18bf0151a4d9b12e51a4ea1e
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-06-19 21:30:17 +0200

    test update

--- src/test/FGlob.test.ts
+++ src/test/FGlob.test.ts
@@ -2,17 +2,23 @@ import FGlob from '../FGlob';
 
 describe('Test for glob features', () => {
 	test('Receive only test1.txt', async () => {
-		const fglob = new FGlob('./src/test/fglobTest', '*.txt');
+		const fglob = new FGlob();
 
-		const paths = await fglob.getPathsToFiles();
+		const paths = await fglob.getPathsToFiles(
+			'./src/test/fglobTest',
+			'*.txt',
+		);
 
 		expect(paths[0]).toEqual('./src/test/fglobTest/test1.txt');
 	});
 
 	test('Receive all (2) test files from fglobTest', async () => {
-		const fglob = new FGlob('./src/test/fglobTest', 'test*');
+		const fglob = new FGlob();
 
-		const paths = await fglob.getPathsToFiles();
+		const paths = await fglob.getPathsToFiles(
+			'./src/test/fglobTest',
+			'test*',
+		);
 
 		expect(paths).toEqual([
 			'./src/test/fglobTest/test1.txt',
@@ -21,9 +27,12 @@ describe('Test for glob features', () => {
 	});
 
 	test('Receive all fast-glob test files', async () => {
-		const fglob = new FGlob('./src/test/fglobTest', '**/*.txt');
+		const fglob = new FGlob();
 
-		const paths = await fglob.getPathsToFiles();
+		const paths = await fglob.getPathsToFiles(
+			'./src/test/fglobTest',
+			'**/*.txt',
+		);
 
 		expect(paths).toEqual([
 			'./src/test/fglobTest/test1.txt',

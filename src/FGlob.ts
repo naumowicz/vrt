761a359175b22b76b74ee6da8501efa0ccf655b6
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-06-19 21:29:56 +0200

    simplyfing FGlob module

--- src/FGlob.ts
+++ src/FGlob.ts
@@ -2,17 +2,15 @@ import * as fglob from 'fast-glob';
 
 class FGlob {
 	pattern = '**/*.txt';
-	pathToFolder: string;
-	arrayOfFiles: string[] = [];
-	constructor(pathToFolder: string, pattern?: string) {
-		this.pathToFolder = pathToFolder;
+
+	public async getPathsToFiles(
+		pathToFolder: string,
+		pattern?: string,
+	): Promise<string[]> {
 		if (pattern != undefined) {
 			this.pattern = pattern;
 		}
-	}
-
-	public async getPathsToFiles(): Promise<string[]> {
-		return await fglob(`${this.pathToFolder}/${this.pattern}`);
+		return await fglob(`${pathToFolder}/${this.pattern}`);
 	}
 }
 

0d87a9d16051756808ac62690d5b2068d553a8c4
Your Name <you@example.com>
2020-07-05 22:11:20 +0200

    configuration for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
 		'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
 		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
 		'plugin:jest/recommended',
+		'plugin:react/recommended'
 	],
 	rules: {
 		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs

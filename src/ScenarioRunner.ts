0e60e4e456dbbff98205d2419e638f4bd9d55095
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-07-06 17:33:03 +0200

    loading Scenario

---
+++ src/ScenarioRunner.ts
@@ -0,0 +1,30 @@
+import Scenario from './Scenario';
+
+class ScenarioRunner {
+	scenario: Scenario;
+	pathToScenario: string;
+
+	constructor(pathToScenario: string) {
+		this.scenario = new Scenario();
+		this.pathToScenario = pathToScenario;
+	}
+
+	loadScenario(): boolean {
+		if (
+			this.scenario.checkScenarioAvailability(this.pathToScenario) ===
+			false
+		) {
+			//log error
+			return false;
+		}
+
+		this.scenario.loadScenario(this.pathToScenario);
+
+		if (this.scenario.isScenarioParsedSuccessfully() === false) {
+			//log error
+			return false;
+		}
+	}
+}
+
+export default ScenarioRunner;

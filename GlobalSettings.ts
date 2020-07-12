eab412add306170db7cdf7465f56446b4377df52
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-07-12 14:04:44 +0200

    loading resemble configs

---
+++ GlobalSettings.ts
@@ -0,0 +1,17 @@
+import { strict } from 'assert';
+
+class GlobalSettings {
+	tasks: string;
+	enableConsoleLogger: boolean;
+	puppeteerConfigPath: string;
+	resembleConfigPath: string;
+
+	constructor() {
+		this.tasks = './tasks.json';
+		this.enableConsoleLogger = true;
+		this.puppeteerConfigPath = './PuppeteerConfig.json';
+		this.resembleConfigPath = './ResembleConfig.json';
+	}
+}
+
+export default new GlobalSettings();
--- src/ImagesComparer.ts
+++ src/ImagesComparer.ts
@@ -1,4 +1,5 @@
 import Resemble from './Resemble';
+import globalSettings from '../GlobalSettings';
 
 class ImagesComparer {
 	async compareImages(
@@ -9,7 +10,7 @@ class ImagesComparer {
 		misMatchPercentage: string;
 		buffer: Buffer;
 	}> {
-		const resemble = new Resemble();
+		const resemble = new Resemble(globalSettings.resembleConfigPath);
 		const result = await resemble.getDiff(originalImage, imageToCompare);
 		const rawMisMatchPercentage = result.rawMisMatchPercentage;
 		const misMatchPercentage = result.misMatchPercentage;

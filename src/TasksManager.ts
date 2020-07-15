c35c7d028577df41d5f2acc2bba0ecc26b286908
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-07-15 15:45:20 +0200

    basic cluster implementation

--- src/TasksManager.ts
+++ src/TasksManager.ts
@@ -1,19 +1,75 @@
 import cluster from 'cluster';
 import { Worker } from 'worker_threads';
+import globalSettings from '../GlobalSettings';
+import ScenarioRunner from './ScenarioRunner';
 
-class TasksManager {
-	logger: Worker;
-	spawnLogger(): void {
-		this.logger = new Worker('./Logger');
+const clusterWorkers: Array<cluster.Worker> = [];
+
+const setUpCluster = (): void => {
+	// const numCores = require('os').cpus().length;
+	const numCores = globalSettings.numberOfThreads;
+	console.log('Master cluster setting up ' + numCores + ' workers');
+	const logger = new Worker('./Logger');
+
+	// iterate on number of cores need to be utilized by an application
+	// current example will utilize all of them
+	for (let i = 0; i < numCores; i++) {
+		// creating workers and pushing reference in an array
+		// these references can be used to receive messages from workers
+		clusterWorkers.push(cluster.fork());
+
+		// to receive messages from worker process
+		clusterWorkers[i].on('message', function (message) {
+			console.log(message);
+		});
 	}
 
-	closeLogger(): void {
-		this.logger.unref();
-	}
+	// process is clustered on a core and process id is assigned
+	cluster.on('online', function (clusterWorker) {
+		console.log('Worker ' + clusterWorker.process.pid + ' is listening');
+	});
 
-	sendLog(type: string, message: string): void {
-		this.logger.postMessage({ type, message });
-	}
-}
+	// if any of the worker process dies then start a new one by simply forking another one
+	cluster.on('exit', function (worker, code, signal) {
+		console.log(
+			'Worker ' +
+				worker.process.pid +
+				' died with code: ' +
+				code +
+				', and signal: ' +
+				signal,
+		);
+		console.log('Starting a new worker');
+		cluster.fork();
+		clusterWorkers.push(cluster.fork());
+		// to receive messages from worker process
+		clusterWorkers[this.clusterWorkers.length - 1].on('message', function (
+			message,
+		) {
+			logger.postMessage(message);
+			// console.log(message);
+		});
+	});
+};
 
-export default TasksManager;
+const startVRT = async (): Promise<void> => {
+	//fixme:
+	const scenarioRunner = new ScenarioRunner('');
+	if (scenarioRunner.loadScenarios()) {
+		await scenarioRunner.runScenarios();
+	} else {
+		return;
+	}
+};
+
+const run = async (): Promise<void> => {
+	// if it is a master process then call setting up worker process
+	if (cluster.isMaster) {
+		setUpCluster();
+	} else {
+		// to setup server configurations and share port address for incoming requests
+		startVRT();
+	}
+};
+
+run();

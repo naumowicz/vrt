39e0a84f9bac0f4b2e218a0b01117d02107b404c
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-07-30 01:45:00 +0200

    fixing resemble unit tests

--- src/test/Resemble.test.ts
+++ src/test/Resemble.test.ts
@@ -1,9 +1,14 @@
 import Resemble from '../Resemble';
 import * as fs from 'fs';
+import globalSettings from '../../GlobalSettings';
+import Path from '../Path';
+const pathToResembleGlobalSettingsFile = globalSettings.resembleConfigPath;
 
 describe('Test for Resemble error area color output', () => {
 	test('Colors are out of scale', () => {
-		const resemble = new Resemble();
+		const resemble = new Resemble(
+			Path.resolvePath(pathToResembleGlobalSettingsFile),
+		);
 		resemble.setErrorOutputColor(-1, 256, undefined);
 
 		expect(resemble.getErrorColor()).toEqual({
@@ -14,7 +19,9 @@ describe('Test for Resemble error area color output', () => {
 	});
 
 	test('Colors are passed properly', () => {
-		const resemble = new Resemble();
+		const resemble = new Resemble(
+			Path.resolvePath(pathToResembleGlobalSettingsFile),
+		);
 		resemble.setErrorOutputColor(1, 200, 20);
 
 		expect(resemble.getErrorColor()).toEqual({
@@ -25,7 +32,9 @@ describe('Test for Resemble error area color output', () => {
 	});
 
 	test('Colors are undefined', () => {
-		const resemble = new Resemble();
+		const resemble = new Resemble(
+			Path.resolvePath(pathToResembleGlobalSettingsFile),
+		);
 		resemble.setErrorOutputColor(undefined, undefined, undefined);
 
 		expect(resemble.getErrorColor()).toEqual({
@@ -38,14 +47,18 @@ describe('Test for Resemble error area color output', () => {
 
 describe('Test for Resemble output transparency', () => {
 	test('Wrong value passed for transparency', () => {
-		const resemble = new Resemble();
+		const resemble = new Resemble(
+			Path.resolvePath(pathToResembleGlobalSettingsFile),
+		);
 		resemble.setTransparency(-1);
 
 		expect(resemble.getTransparencyLevel()).toEqual(0.3);
 	});
 
 	test('Proper value passed for transparency', () => {
-		const resemble = new Resemble();
+		const resemble = new Resemble(
+			Path.resolvePath(pathToResembleGlobalSettingsFile),
+		);
 		resemble.setTransparency(0.4);
 
 		expect(resemble.getTransparencyLevel()).toEqual(0.4);
@@ -54,16 +67,19 @@ describe('Test for Resemble output transparency', () => {
 
 describe('Testing Resemblejs comparation with default settings', () => {
 	test('Images are the same', async () => {
-		const resemble = new Resemble();
+		const resemble = new Resemble(
+			Path.resolvePath(pathToResembleGlobalSettingsFile),
+		);
 
 		const image1 = fs.readFileSync('./src/test/testimage1.png');
 		const image2 = fs.readFileSync('./src/test/testimage1.png');
 
 		const data = await resemble.getDiff(image1, image2);
 
-		// eslint-disable-next-line prettier/prettier
-		const resultBuffer = fs.readFileSync('./src/test/buffer_test_images_equal.txt');
+		const resultBuffer = fs.readFileSync(
+			'./src/test/buffer_test_images_equal.png',
+		);
 
-		expect(data.getBuffer()).toEqual(resultBuffer);
+		expect(data.getBuffer().equals(resultBuffer)).toEqual(true);
 	});
 });

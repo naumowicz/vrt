87bd8024f6e9004540f31bee5ce9db044a22ec12
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-08-01 15:36:57 +0200

    test for readFile

--- src/test/FileSystem.test.ts
+++ src/test/FileSystem.test.ts
@@ -10,7 +10,7 @@ describe('Testing FileSystem', () => {
 			expect(result).toEqual(true);
 		});
 		test('File does not exists yet', () => {
-			const file = './src/test/fileSystemTest/doesNotExists.txt';
+			const file = './src/test/fileSystemTest/doesNotExistsYet.txt';
 			const result = FileSystem.writeFile(file, Buffer.alloc(0));
 			expect(result).toEqual(true);
 			expect(FileSystem.checkAvailability(file)).toEqual(true);
@@ -18,6 +18,23 @@ describe('Testing FileSystem', () => {
 			//cleanup
 			FileSystem.removeFolderRecursively(file);
 			expect(FileSystem.checkAvailability(file)).toEqual(false);
+		});
+	});
+
+	describe('Test for readFile', () => {
+		test('File does not exists', () => {
+			const result = FileSystem.readFile(
+				'./src/test/fileSystemTest/doesNotExists.txt',
+			);
+			expect(result.status).toEqual(false);
+			expect(result.fileContent).toEqual(Buffer.from(''));
+		});
+		test('File exists', () => {
+			const result = FileSystem.readFile(
+				'./src/test/fileSystemTest/fileExists.txt',
+			);
+			expect(result.status).toEqual(true);
+			expect(result.fileContent).toEqual(Buffer.from('content of file'));
 		});
 	});
 });

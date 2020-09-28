e121c323f152e21d6b7cfc6eee2993bdfe33a907
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-09-29 00:28:49 +0200

    commenting out useless method and added debug color

--- src/ConsoleLogger.ts
+++ src/ConsoleLogger.ts
@@ -4,24 +4,32 @@ class ConsoleLogger {
 	errorColor: string;
 	warningColor: string;
 	infoColor: string;
+	debugColor: string;
 
 	constructor(
 		errorColor = colors.FgRed,
 		warningColor = colors.FgYellow,
 		infoColor = colors.FgWhite,
+		debugColor = colors.FgGreen,
 	) {
-		this.setColors(errorColor, warningColor, infoColor);
-	}
-
-	setColors(
-		errorColor: string,
-		warningColor: string,
-		infoColor: string,
-	): void {
 		this.errorColor = errorColor;
 		this.warningColor = warningColor;
 		this.infoColor = infoColor;
+		this.debugColor = debugColor;
+		// this.setColors(errorColor, warningColor, infoColor, debugColor);
 	}
+
+	// setColors(
+	// 	errorColor: string,
+	// 	warningColor: string,
+	// 	infoColor: string,
+	// 	debugColor: string,
+	// ): void {
+	// 	this.errorColor = errorColor;
+	// 	this.warningColor = warningColor;
+	// 	this.infoColor = infoColor;
+	// 	this.debugColor = debugColor;
+	// }
 
 	error(data: string): void {
 		console.log(`${this.errorColor}${data}${colors.Reset}`);
@@ -33,6 +41,10 @@ class ConsoleLogger {
 
 	info(data: string): void {
 		console.log(`${this.infoColor}${data}${colors.Reset}`);
+	}
+
+	debug(data: string): void {
+		console.log(`${this.debugColor}${data}${colors.Reset}`);
 	}
 }
 

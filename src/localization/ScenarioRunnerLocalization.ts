22331c987060c1e1477e8e09ad2f54687e5b8c4a
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-09-29 14:02:40 +0200

    reordering

--- src/localization/ScenarioRunnerLocalization.ts
+++ src/localization/ScenarioRunnerLocalization.ts
@@ -1,22 +1,30 @@
 const scenarioRunnerLocalization = {
-	startingScenarioRunner: 'Starting scenario: ',
-	loadingScenario: 'Loading scenario: ',
+	//error
 	scenarioNotAvailable: 'Scenario was not availiable under this path: ',
 	failedLoadingScenario: 'Failed parsing scenario: ',
-	errorsFromParsingScenario: 'Received errors from parsing scenario',
-	loadingSuccessfull: 'Loading scenario was successful',
-	runningScenario: 'Running scenario: ',
 	creatingFoldersFail: 'Creating folders for images was unsuccessfull',
-	openingPuppeteer: 'Opening puppeteer',
-	runningStepsInPuppeteer: 'Running steps in a puppeteer',
-	closingPuppeteer: 'Closing puppeteer',
-	scenarioFinished: 'Scenario finished successfully',
-	createFoldersNoImagesToAnalyze: 'Received 0 images to analyze',
 	creatingBaselineFolderNotPossible:
 		'Unable to create baseline folder for path: ',
 	recreatingOutputAndActualStatusFolderNotPossible:
 		'Unable to create actualStatus or output folder for path: ',
+
+	//warning
+	createFoldersNoImagesToAnalyze: 'Received 0 images to analyze',
+
+	//info
+	startingScenarioRunner: 'Starting scenario: ',
+	runningScenario: 'Running scenario: ',
+	//debug
+	loadingScenario: 'Loading scenario: ',
+	loadingSuccessful: 'Loading scenario was successful',
+	openingPuppeteer: 'Opening puppeteer',
+	runningStepsInPuppeteer: 'Running steps in a puppeteer',
+	closingPuppeteer: 'Closing puppeteer',
+	scenarioFinished: 'Scenario finished successfully',
 	creatingFoldersSuccessful: 'Creating folders was successful',
+
+	errorsFromParsingScenario: 'Received errors from parsing scenario',
+
 	analyzingStep: 'Analyzing step:',
 	puppeteerVisitsPage: 'Puppeteer visits page: ',
 	checkingIfBaselineExists: 'Checking if baseline is available: ',

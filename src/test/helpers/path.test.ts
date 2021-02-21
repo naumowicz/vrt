34130306cc1e488ffea63f3ce71f64ffea3cec1e
Mateusz Naumowicz <naumowicz@protonmail.com>
2021-02-21 14:43:10 +0100

    new tests

--- src/test/helpers/path.test.ts
+++ src/test/helpers/path.test.ts
@@ -62,15 +62,102 @@ describe('Testing hasExtension', () => {
 
 describe('Testing getExtensions', () => {
 	test('empty path', () => {
-		expect(path.getExtensions('')).toEqual({success: false, data: ''});
+		expect(path.getExtensions('')).toEqual({ success: false, data: '' });
 	});
 	test('path to folder', () => {
-		expect(path.getExtensions('/test/folder')).toEqual({success: false, data: ''});
+		expect(path.getExtensions('/test/folder')).toEqual({ success: false, data: '' });
 	});
 	test('path with / at the end', () => {
-		expect(path.getExtensions('/test/folder/')).toEqual({success: false, data: ''});
+		expect(path.getExtensions('/test/folder/')).toEqual({ success: false, data: '' });
 	});
 	test('path with extension', () => {
-		expect(path.getExtensions('/test/folder/file.txt')).toEqual({success: true, data: '.txt'});
+		expect(path.getExtensions('/test/folder/file.txt')).toEqual({ success: true, data: '.txt' });
+	});
+});
+
+describe('Testing convertToPosix', () => {
+	test('empty path', () => {
+		expect(path.convertToPosix('')).toEqual('');
+	});
+	test('just /', () => {
+		expect(path.convertToPosix('/')).toEqual('/');
+	});
+	test('path to folder', () => {
+		expect(path.convertToPosix('/test/folder')).toEqual('/test/folder');
+	});
+	test('path with / at the end', () => {
+		expect(path.convertToPosix('/test/folder/')).toEqual('/test/folder/');
+	});
+	test('path with extension', () => {
+		expect(path.convertToPosix('/test/folder/file.txt')).toEqual('/test/folder/file.txt');
+	});
+	test('Windows path', () => {
+		expect(path.convertToPosix('C:\\test\\folder')).toEqual('C:/test/folder');
+	});
+	test('Windows path with \\ at the end', () => {
+		expect(path.convertToPosix('C:\\test\\folder\\')).toEqual('C:/test/folder/');
+	});
+	test('Windows path with extension', () => {
+		expect(path.convertToPosix('C:\\test\\folder\\file.txt')).toEqual('C:/test/folder/file.txt');
+	});
+});
+
+describe('Testing parsePath', () => {
+	test('empty path', () => {
+		expect(path.parsePath('')).toEqual({ root: '', dir: '', base: '', ext: '', name: '' });
+	});
+	test('path to folder', () => {
+		expect(path.parsePath('/test/folder')).toEqual({
+			root: '/',
+			dir: '/test',
+			base: 'folder',
+			ext: '',
+			name: 'folder',
+		});
+	});
+	test('path with / at the end', () => {
+		expect(path.parsePath('/test/folder/')).toEqual({
+			root: '/',
+			dir: '/test',
+			base: 'folder',
+			ext: '',
+			name: 'folder',
+		});
+	});
+	test('path with extension', () => {
+		expect(path.parsePath('/test/folder/file.txt')).toEqual({
+			root: '/',
+			dir: '/test/folder',
+			base: 'file.txt',
+			ext: '.txt',
+			name: 'file',
+		});
+	});
+	test('Windows path with extension', () => {
+		expect(path.parsePath('C:/test/folder/file.txt')).toEqual({
+			root: '',
+			dir: 'C:/test/folder',
+			base: 'file.txt',
+			ext: '.txt',
+			name: 'file',
+		});
+	});
+	test('Windows path', () => {
+		expect(path.parsePath('C:/test/folder')).toEqual({
+			root: '',
+			dir: 'C:/test',
+			base: 'folder',
+			ext: '',
+			name: 'folder',
+		});
+	});
+	test('Windows path with / at the end', () => {
+		expect(path.parsePath('C:/test/folder/')).toEqual({
+			root: '',
+			dir: 'C:/test',
+			base: 'folder',
+			ext: '',
+			name: 'folder',
+		});
 	});
 });

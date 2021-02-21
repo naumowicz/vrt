5599fe248d6c25b15974bded0b65ed1b0ced0d73
Mateusz Naumowicz <naumowicz@protonmail.com>
2021-02-21 01:57:01 +0100

    test update

--- src/test/helpers/path.test.ts
+++ src/test/helpers/path.test.ts
@@ -7,10 +7,25 @@ describe('Testing getFolder', () => {
 	test('proper path', () => {
 		expect(path.getFoler('/test/folder')).toEqual({success: true, data: 'folder'})
 	});
-	test('proper path with / at the end', () => {
+	test('path with / at the end', () => {
 		expect(path.getFoler('/test/folder/')).toEqual({success: true, data: 'folder'})
 	});
 	test('path with extension', () => {
 		expect(path.getFoler('/test/folder/file.txt')).toEqual({success: false, data: ''})
 	});
 });
+
+describe('Testing hasExtenstion', () => {
+	test('empty path', () => {
+		expect(path.hasExtension('')).toEqual(false)
+	});
+	test('proper path', () => {
+		expect(path.hasExtension('/test/folder')).toEqual(false)
+	});
+	test('path with / at the end', () => {
+		expect(path.hasExtension('/test/folder/')).toEqual(false)
+	});
+	test('path with extension', () => {
+		expect(path.hasExtension('/test/folder/file.txt')).toEqual(true)
+	});
+})

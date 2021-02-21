b160f950d208bd02fa1bba69b6f87c270131729d
Mateusz Naumowicz <naumowicz@protonmail.com>
2021-02-21 02:15:54 +0100

    update - new tests

--- src/test/helpers/path.test.ts
+++ src/test/helpers/path.test.ts
@@ -29,3 +29,33 @@ describe('Testing hasExtenstion', () => {
 		expect(path.hasExtension('/test/folder/file.txt')).toEqual(true);
 	});
 });
+
+describe('Testing getFile', () => {
+	test('empty path', () => {
+		expect(path.getFile('')).toEqual({ success: false, data: '' });
+	});
+	test('path to folder', () => {
+		expect(path.getFile('/test/folder')).toEqual({ success: false, data: '' });
+	});
+	test('path with / at the end', () => {
+		expect(path.getFile('/test/folder/')).toEqual({ success: false, data: '' });
+	});
+	test('path with extension', () => {
+		expect(path.getFile('/test/folder/file.txt')).toEqual({ success: true, data: 'file.txt' });
+	});
+});
+
+describe('Testing hasExtension', () => {
+	test('empty path', () => {
+		expect(path.hasExtension('')).toEqual(false);
+	});
+	test('path to folder', () => {
+		expect(path.hasExtension('/test/folder')).toEqual(false);
+	});
+	test('path with / at the end', () => {
+		expect(path.hasExtension('/test/folder/')).toEqual(false);
+	});
+	test('path with extension', () => {
+		expect(path.hasExtension('/test/folder/file.txt')).toEqual(true);
+	});
+});

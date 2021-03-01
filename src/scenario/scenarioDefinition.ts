5c15654cecd3f0b80e33140ad8d5ea0a5ad6a23b
Mateusz Naumowicz <naumowicz@protonmail.com>
2021-03-01 01:14:39 +0100

    class for checking types of loaded JSON

---
+++ src/scenario/scenarioDefinition.ts
@@ -0,0 +1,82 @@
+interface scenarioDefinition {
+	browser: string;
+	resolution: RegExp;
+	headless: boolean;
+	autosavingBaseline: boolean;
+	ignoringAlgorithm: string;
+	errorType: string;
+	errorColor: {
+		red: number;
+		green: number;
+		blue: number;
+	};
+	transparency: number;
+	largeImageThreshold: number;
+	setReturnEarlyThreshold: number | undefined; //be sure about checking type
+	scaleToSameSize: boolean;
+	scenarioTasks: Array<ScenarioTask>;
+}
+
+interface ScenarioTask {
+	tag: [string];
+	boundingBoxes: [Box | Record<string, never>];
+	ignoredBoxes: [Box | Record<string, never>];
+	script: null | string;
+	actions: [Action];
+}
+
+interface Box {
+	left: number;
+	top: number;
+	right: number;
+	bottom: number;
+}
+
+interface Action {
+	action: string;
+	target: string;
+	value: string;
+}
+
+class scenarioRules {
+	browser: Array<string> = ['chrome'];
+	resolution = /\d+x\d+/;
+	ignoringAlgorithm: Array<string> = ['nothing', 'less', 'antialiasing', 'colors', 'alpha'];
+	errorType: Array<string> = [
+		'flat',
+		'movement',
+		'flatDifferenceIntensity',
+		'movementDifferenceIntensity',
+		'diffOnly',
+	];
+	scenario: unknown;
+	constructor(scenario: unknown) {
+		this.scenario = scenario;
+	}
+
+	isUsingProperBrowser(): boolean {
+		const expectedScenarioType = this.scenario as scenarioDefinition;
+		return 'browser' in expectedScenarioType ? this.browser.includes(expectedScenarioType.browser) : false;
+	}
+
+	isUsingProperResolutionFormat(): boolean {
+		const expectedScenarioType = this.scenario as scenarioDefinition;
+		return 'resolution' in expectedScenarioType
+			? this.resolution.test(expectedScenarioType.resolution.toString())
+			: false;
+	}
+
+	isUsingProperIgnoringAlghoritm(): boolean {
+		const expectedScenarioType = this.scenario as scenarioDefinition;
+		return 'ignoringAlgorithm' in expectedScenarioType
+			? this.ignoringAlgorithm.includes(expectedScenarioType.ignoringAlgorithm)
+			: false;
+	}
+
+	isUsingProperErrorType(): boolean {
+		const expectedScenarioType = this.scenario as scenarioDefinition;
+		return 'errorType' in expectedScenarioType ? this.errorType.includes(expectedScenarioType.errorType) : false;
+	}
+}
+
+export default scenarioRules;

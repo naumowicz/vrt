5c291fefe0850408884abe6f49f3169db7bfd842
Mateusz Naumowicz <naumowicz@protonmail.com>
2021-03-02 23:40:38 +0100

    removing else, simplyfing

--- src/scenario/scenario.ts
+++ src/scenario/scenario.ts
@@ -26,34 +26,27 @@ class Scenario {
 	checkScenario(): boolean {
 		const scenarioDefinition = new ScenarioDefinition();
 		if (scenarioDefinition.isUsingProperBrowser(this.scenario)) {
-			//ok browser
-		} else {
 			//wrong browser
 			return false;
 		}
 
 		if (scenarioDefinition.isUsingProperResolutionFormat(this.scenario)) {
-			//ok resolution
-		} else {
 			//wrong resolution
 			return false;
 		}
 
-		if (scenarioDefinition.isUsingProperIgnoringAlghoritm(this.scenario)) {
-			//ok ignoring algorithm
-		} else {
+		if (scenarioDefinition.isUsingProperIgnoringAlghoritm(this.scenario) === false) {
 			//wrong ignoring algorithm
 			return false;
 		}
 
-		if (scenarioDefinition.isUsingProperErrorType(this.scenario)) {
-			//ok error type
-			//all ok
-			return true;
-		} else {
-			//wrong error type
+		if (scenarioDefinition.isUsingProperErrorType(this.scenario) === false) {
+			//checking error type
 			return false;
 		}
+
+		//all ok
+		return true;
 	}
 }
 
